8c4157cbe8d8cfbbeb7f8375aed108a3c50a9cfd
guido turnes <turnesg@MacBook-Pro-de-guido.local>
2021-12-25 15:01:45 -0300

    Primer alta

---
+++ dist/index.49059b93.js
@@ -0,0 +1,20 @@
+document.querySelector(".request-complement").addEventListener("click", function() {
+    fetch("/complement").then(function(res) {
+        return res.json();
+    }).then(function(data) {
+        document.querySelector(".complement").innerText = data.complement;
+    }).catch(function(err) {
+        console.error(err);
+    });
+});
+document.querySelector(".request-insult").addEventListener("click", function() {
+    fetch("/insult").then(function(res) {
+        return res.json();
+    }).then(function(data) {
+        document.querySelector(".complement").innerText = data.insult;
+    }).catch(function(err) {
+        console.error(err);
+    });
+});
+
+//# sourceMappingURL=index.49059b93.js.map
